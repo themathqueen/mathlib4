5a21318753c89d30f18d3c1a5429fc38c7166df9
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-01-30 19:31:24 +0000

    Update Mathlib/Combinatorics/SimpleGraph/AdjMatrix.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Combinatorics/SimpleGraph/AdjMatrix.lean
+++ Mathlib/Combinatorics/SimpleGraph/AdjMatrix.lean
@@ -231,7 +231,7 @@ theorem adjMatrix_mul_self_apply_self [NonAssocSemiring α] (i : V) :
 variable (R) in
 /-- The number of all edges in a simple finite graph is equal to the dot product of
 `G.adjMatrix α *ᵥ 1` and `1`. -/
-theorem coe_card_dart_eq_dotProduct [NonAssocSemiring α] :
+theorem natCast_card_dart_eq_dotProduct [NonAssocSemiring α] :
     Fintype.card G.Dart = adjMatrix α G *ᵥ 1 ⬝ᵥ 1 := by
   simp [G.dart_card_eq_sum_degrees, dotProduct_one]
 
