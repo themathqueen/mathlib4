b9b1d6f23fedb27d101ac514905368c85f70c7c2
Monica Omar <2497250a@research.gla.ac.uk>
2026-02-13 01:18:50 +0000

    move to file

--- Mathlib.lean
+++ Mathlib.lean
@@ -4786,6 +4786,7 @@ public import Mathlib.LinearAlgebra.Matrix.Trace
 public import Mathlib.LinearAlgebra.Matrix.Transvection
 public import Mathlib.LinearAlgebra.Matrix.Unique
 public import Mathlib.LinearAlgebra.Matrix.Vec
+public import Mathlib.LinearAlgebra.Matrix.WithConv
 public import Mathlib.LinearAlgebra.Matrix.ZPow
 public import Mathlib.LinearAlgebra.Multilinear.Basic
 public import Mathlib.LinearAlgebra.Multilinear.Basis
